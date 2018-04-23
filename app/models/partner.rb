8b40b79e4fd3c31cb84aa8a34af1e43a0b11a294
divyajayan <divya.jayan@quantiguous.com>
2018-04-23 16:43:10 +0530

    SY-380 validations in partner setup with FCR/ATOM

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -106,28 +106,26 @@ class Partner < ActiveRecord::Base
   end
   
   def should_allow_neft?
-    return true
-    # fcr_customer = Fcr::Customer.find_by_cod_cust_id(self.customer_id)
-    # if fcr_customer.nil?
-    #   errors.add(:customer_id, "no record found in FCR for #{self.customer_id}")
-    # else
-    #   errors.add(:allow_neft, "NEFT is not allowed for #{self.customer_id} as the data setup in FCR is invalid") unless fcr_customer.transfer_type_allowed?('NEFT')
-    # end
+    fcr_customer = Fcr::Customer.find_by_cod_cust_id(self.customer_id)
+    if fcr_customer.nil?
+      errors.add(:customer_id, "no record found in FCR for #{self.customer_id}")
+    else
+      errors.add(:allow_neft, "NEFT is not allowed for #{self.customer_id} as the data setup in FCR is invalid") unless fcr_customer.transfer_type_allowed?('NEFT')
+    end
   end
   
   def should_allow_imps?
-    return true
-    # fcr_customer = Fcr::Customer.find_by_cod_cust_id(self.customer_id)
-    # atom_customer_by_cust_id = Atom::Customer.find_by_customerid(self.customer_id)
-    # atom_customer_by_debit_acct = Atom::Customer.find_by_accountno(self.account_no)
-    #
-    # if fcr_customer.present? && atom_customer_by_cust_id.present? && atom_customer_by_debit_acct.present?
-    #   errors.add(:account_no, "IMPS is not allowed for #{self.account_no} as the data setup in ATOM is invalid") unless atom_customer_by_debit_acct.imps_allowed?(fcr_customer.ref_phone_mobile)
-    # else
-    #   errors.add(:customer_id, "no record found in FCR for #{self.customer_id}") if fcr_customer.nil?
-    #   errors.add(:customer_id, "no record found in ATOM for #{self.customer_id}") if atom_customer_by_cust_id.nil?
-    #   errors.add(:account_no, "no record found in ATOM for #{self.account_no}") if atom_customer_by_debit_acct.nil?
-    # end
+    fcr_customer = Fcr::Customer.find_by_cod_cust_id(self.customer_id)
+    atom_customer_by_cust_id = Atom::Customer.find_by_customerid(self.customer_id)
+    atom_customer_by_debit_acct = Atom::Customer.find_by_accountno(self.account_no)
+
+    if fcr_customer.present? && atom_customer_by_cust_id.present? && atom_customer_by_debit_acct.present?
+      errors.add(:account_no, "IMPS is not allowed for #{self.account_no} as the data setup in ATOM is invalid") unless atom_customer_by_debit_acct.imps_allowed?(fcr_customer.ref_phone_mobile)
+    else
+      errors.add(:customer_id, "no record found in FCR for #{self.customer_id}") if fcr_customer.nil?
+      errors.add(:customer_id, "no record found in ATOM for #{self.customer_id}") if atom_customer_by_cust_id.nil?
+      errors.add(:account_no, "no record found in ATOM for #{self.account_no}") if atom_customer_by_debit_acct.nil?
+    end
   end
 
   def template_variables
